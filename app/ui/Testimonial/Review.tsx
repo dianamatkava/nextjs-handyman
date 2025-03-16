dd185d685f6576ae03898560d3cad3a73c233c86
dianamatkava <dianamatkava@gmail.com>
2025-03-16 18:07:21 +0100

    Fix vercel issues

--- app/ui/Testimonial/Review.tsx
+++ app/ui/Testimonial/Review.tsx
@@ -19,7 +19,7 @@ export default function Review({
   return (
     <div className="w-fit h-[200px] p-5 rounded-md border border-[#eaeaea] flex-col justify-start items-start gap-3 inline-flex">
       <div className="justify-start items-center gap-5 inline-flex">
-        <Profile name={name} title={service} image={image} />
+        <Profile name={name} title={service ?? ""} image={image} />
         <StartRating rating={rating} />
       </div>
       <div className="w-[350px] text-black text-xs font-normal font-['Plus Jakarta Sans'] tracking-tight">
