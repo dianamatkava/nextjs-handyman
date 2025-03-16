4a9410139b6fb0e5d59eafc547a47eb282d404cc
dianamatkava <dianamatkava@gmail.com>
2025-03-16 17:47:52 +0100

    Fix btn

--- app/ui/Components/CTAGroup.tsx
+++ app/ui/Components/CTAGroup.tsx
@@ -1,10 +1,15 @@
 import CTAPrimary from "@/app/ui/Components/CTAPrimary";
 import CTACall from "@/app/ui/Components/CTACall";
 
-export default function CTAGroup({ children = null, className = "" }) {
+interface CTAGroupProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function CTAGroup({ children, className }: CTAGroupProps) {
   return (
     <div
-      className={`flex-col justify-center items-center gap-4 flex ${className}`}
+      className={`w-full h-fit flex-col justify-start items-start gap-5 inline-flex ${className}`}
     >
       <div className="self-stretch justify-center items-center gap-4 inline-flex">
         <CTAPrimary className={"bg-[#fe7800]"} />
