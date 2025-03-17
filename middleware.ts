62bd235b6d214f2ab47332d41b74621972f478bd
dianamatkava <dianamatkava@gmail.com>
2025-03-17 08:31:04 +0100

    fix redirect

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,3 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-  const hostname = request.headers.get("host") || "";
-
-  if (hostname === "alxhandyman.com" && process.env.NODE_ENV === "production") {
-    url.protocol = "https";
-    url.host = `www.${hostname}`;
-    return NextResponse.redirect(url);
-  }
-
-  // Remove trailing slashes
-  if (url.pathname.endsWith("/") && url.pathname.length > 1) {
-    url.pathname = url.pathname.slice(0, -1);
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
-}
-
 export const config = {
-  matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api routes
-     * 2. /_next (Next.js internals)
-     * 3. /static (inside /public)
-     * 4. all root files inside /public (e.g. /favicon.ico)
-     */
-    "/((?!api|_next|static|[\\w-]+\\.\\w+).*)",
-  ],
+  matcher: ["/((?!api|_next|static|[\\w-]+\\.\\w+).*)"],
 };
